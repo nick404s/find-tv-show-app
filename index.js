51fbc5164148dab7335d801f9af1b79b1c16da63
Nikolay Skryabin <nickskryabin@gmail.com>
2023-05-08 12:36:55 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,150 @@
+// https://api.tvmaze.com/singlesearch/shows?q=girls
+const tvmazeAPISearch = 'https://api.tvmaze.com/search/shows?q=';
+const tvmazeAPISingleSearch = 'https://api.tvmaze.com/singlesearch/shows?q=';
+
+// get the DOM elements
+const findForm = document.querySelector('#find-form');
+const findInput = document.querySelector('#find-input');
+const findResult = document.querySelector('#find-result');
+const showInfo = document.querySelector('#show-info');
+
+
+// Gets the TV shows data from the API
+const getTVShowData = async url => {
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error('Request error: ', error);
+    }
+};
+
+// Displays the shows titles and images
+const displayTVShows = showsData => {
+
+    showsData.forEach(element => {
+
+        // check if the show has image and name
+        if (element.show.image && element.show.name) {
+            
+            // create the image box div to store the show image and name
+            const imageBox  = document.createElement('div');
+
+            imageBox.className = 'image-box';
+            // create the image, name and info button for each show element
+            imageBox.innerHTML = `<h3>${element.show.name}</h3>
+                                    <img src="${element.show.image.medium}">
+                                    <button class="info-btn" data-name="${element.show.name}" 
+                                    data-id="${element.show.id}">Get Info</button>
+                                    `;
+
+            //append the image box for display 
+            findResult.append(imageBox);           
+        }
+    });
+
+};
+
+// Displays the show info
+const displayShowInfo = show => {
+
+    showInfo.innerHTML = `<div id="info">
+                                <h3>${show.name}</h3>
+                                <img src="${show.image.medium}">
+                                <p>Date of Release: ${show.premiered}</p>
+                                <p>Genres: ${show.genres.join(', ')}</p>
+                                <p>Summary: <br>${show.summary}</p>
+                                <button id="return-btn">Return</button>
+                            </div>
+                            `;
+};
+
+// Displays the error message
+const displayError = message => {
+    findResult.innerHTML = `<h3 class="error">${message}</h3>`;
+};
+
+// Creates event listener for the info buttons
+const createShowInfoButtonsEvent = () => {
+
+    const buttons = document.querySelectorAll('.info-btn');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', async evt => {
+
+            // hide the tv shows search results
+            findResult.classList.add('hide');
+
+            // get the selected show name
+            const showName = evt.target.getAttribute('data-name');
+
+            // construct the url for the selected show
+            const url = `${tvmazeAPISingleSearch}${showName}`;
+
+            const data = await getTVShowData(url);
+
+            displayShowInfo(data);
+
+            // create event for the return button
+            createReturnButtonEvent();
+        });
+    });
+
+};
+
+// Creates event listener for the return button
+const createReturnButtonEvent = () => {
+
+    const returnBtn = document.querySelector('#return-btn');
+
+    returnBtn.addEventListener('click', () => {
+
+        // clear the show info
+        showInfo.innerHTML = '';
+
+        // show the tv shows again
+        findResult.classList.remove('hide');
+    });
+}
+
+// Event listener for the find form
+findForm.addEventListener('submit', async evt => {
+
+    // prevent the default form submission
+    evt.preventDefault();
+
+    // reset the values of the show info and search result
+    showInfo.innerHTML = '';
+    findResult.innerHTML = '';
+
+    // show the search results div
+    findResult.classList.remove('hide');
+
+    const inputText = findInput.value.trim();
+
+    // check if the input is empty
+    if (!inputText) {
+        displayError('Please, Enter TV Show Name');
+        return;
+    }
+    
+    const data = await getTVShowData(`${tvmazeAPISearch}${inputText}`);
+
+    //show the error message if the data is empty
+    if (data === null || data.length === 0) {
+        displayError('Sorry, Couldn\'t Find the TV Show');
+        return;
+    }
+
+    displayTVShows(data); 
+
+    // create event listener for the get info buttons
+    createShowInfoButtonsEvent();
+
+    // reset the input 
+    findInput.value = '';
+});
+
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Find TV Shows App
+Find TV Shows and their info at [tvmaze](https://www.tvmaze.com/api) API
+
+## Project Specifications
+- Display UI with TV Show Title input
+- Fetch the shows names and images and put in DOM
+- Add get info functionality and display in DOM
